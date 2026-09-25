785bc9ce6cfad4a0e1c6fe0ae94e063a61a9ef89
shamiulriyad <shamiulriyad1418@gmail.com>
2026-09-26 02:26:07 +0600

    fix frontend and backend bug

--- backend/src/ShilpoHubBD.Application/Interfaces/Services/ICustomOrderService.cs
+++ backend/src/ShilpoHubBD.Application/Interfaces/Services/ICustomOrderService.cs
@@ -9,5 +9,7 @@ public interface ICustomOrderService
     Task<List<CustomOrderRequestDto>> GetMineAsProducerAsync(Guid producerId, CancellationToken cancellationToken);
     Task<CustomOrderRequestDto> CreateAsync(Guid customerId, CreateCustomOrderRequest request, CancellationToken cancellationToken);
     Task<CustomOrderRequestDto> RespondAsync(Guid id, Guid producerId, bool isAdmin, RespondToCustomOrderRequest request, CancellationToken cancellationToken);
+    Task<CustomOrderRequestDto> UpdateDeliveryAsync(Guid id, Guid customerId, UpdateCustomOrderDeliveryRequest request, CancellationToken cancellationToken);
+    Task<CustomOrderRequestDto> ShipAsync(Guid id, Guid producerId, bool isAdmin, ShipCustomOrderRequest request, CancellationToken cancellationToken);
     Task<CustomOrderRequestDto> CancelAsync(Guid id, Guid customerId, CancellationToken cancellationToken);
 }
