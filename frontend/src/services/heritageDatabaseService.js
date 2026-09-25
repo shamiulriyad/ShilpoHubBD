2e1c35ca85af5003abee8c9de7226f428b1fd020
shamiulriyad <shamiulriyad1418@gmail.com>
2026-09-26 00:03:21 +0600

    fix some problem

--- frontend/src/services/heritageDatabaseService.js
+++ frontend/src/services/heritageDatabaseService.js
@@ -6,12 +6,20 @@ export const heritageDatabaseService = {
   getVillages: (params) => apiClient.get('/heritage-database/live/villages', { params }).then((res) => res.data),
   getProducers: (params) => apiClient.get('/heritage-database/live/producers', { params }).then((res) => res.data),
 
+  getProducts: (params) => apiClient.get('/heritage-database/live/products', { params }).then((res) => res.data),
+  getTourism: (params) => apiClient.get('/heritage-database/live/tourism', { params }).then((res) => res.data),
+  getDemographics: () => apiClient.get('/heritage-database/live/demographics').then((res) => res.data),
+
   listDatasets: (params) => apiClient.get('/heritage-database/datasets', { params }).then((res) => res.data),
   getDataset: (id) => apiClient.get(`/heritage-database/datasets/${id}`).then((res) => res.data),
   createDataset: (payload) => apiClient.post('/heritage-database/datasets', payload).then((res) => res.data),
   updateDataset: (id, payload) => apiClient.put(`/heritage-database/datasets/${id}`, payload).then((res) => res.data),
   refreshDataset: (id) => apiClient.post(`/heritage-database/datasets/${id}/refresh`, {}).then((res) => res.data),
   removeDataset: (id) => apiClient.delete(`/heritage-database/datasets/${id}`).then((res) => res.data),
+
+  exportDataset: (id, payload) => apiClient.post(`/heritage-database/datasets/${id}/exports`, payload).then((res) => res.data),
+  listDatasetExports: (id, params) => apiClient.get(`/heritage-database/datasets/${id}/exports`, { params }).then((res) => res.data),
+  getExportAnalytics: (id) => apiClient.get(`/heritage-database/datasets/${id}/export-analytics`).then((res) => res.data),
 
   listRiskRecords: (params) => apiClient.get('/heritage-database/risk', { params }).then((res) => res.data),
   createRiskRecord: (payload) => apiClient.post('/heritage-database/risk', payload).then((res) => res.data),
