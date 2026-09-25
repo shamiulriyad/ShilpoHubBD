4677bcb793497ed0a3c7bdb98598fb07a24a9c65
shamiulriyad <shamiulriyad1418@gmail.com>
2026-09-26 02:25:58 +0600

    fix frontend and backend bug

--- backend/src/ShilpoHubBD.Application/DTOs/Community/CreateQuestionRequest.cs
+++ backend/src/ShilpoHubBD.Application/DTOs/Community/CreateQuestionRequest.cs
@@ -3,4 +3,5 @@ namespace ShilpoHubBD.Application.DTOs.Community;
 public class CreateQuestionRequest
 {
     public string Body { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
 }
