498b02d0b1312ed46b11697eb84ecfa3e6a54812
shamiulriyad <shamiulriyad1418@gmail.com>
2026-09-26 04:56:23 +0600

    add new method in backend and frontend

--- backend/src/ShilpoHubBD.Application/Interfaces/Services/IUserProfileService.cs
+++ backend/src/ShilpoHubBD.Application/Interfaces/Services/IUserProfileService.cs
@@ -6,6 +6,7 @@ namespace ShilpoHubBD.Application.Interfaces.Services;
 public interface IUserProfileService
 {
     Task<UserProfileDto> GetMineAsync(Guid userId, CancellationToken cancellationToken);
+    Task<UserProfileDto> SetPhotoAsync(Guid userId, string? photoUrl, CancellationToken cancellationToken);
     Task<UserProfileDto> UpsertMineAsync(Guid userId, UpsertUserProfileRequest request, CancellationToken cancellationToken);
 
     Task<PagedResult<UserProfileListItemDto>> GetForAdminAsync(UserProfileQueryParameters query, CancellationToken cancellationToken);
