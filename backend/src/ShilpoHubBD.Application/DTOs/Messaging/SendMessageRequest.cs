3bfc1abd0e79befd175299ca078290a511eeb5ec
shamiulriyad <shamiulriyad1418@gmail.com>
2026-09-26 02:26:01 +0600

    fix frontend and backend bug

--- backend/src/ShilpoHubBD.Application/DTOs/Messaging/SendMessageRequest.cs
+++ backend/src/ShilpoHubBD.Application/DTOs/Messaging/SendMessageRequest.cs
@@ -3,4 +3,5 @@ namespace ShilpoHubBD.Application.DTOs.Messaging;
 public class SendMessageRequest
 {
     public string Body { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
 }
