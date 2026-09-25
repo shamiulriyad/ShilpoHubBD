489198e3080c84c1cc9280a58bbbf72c1aa85ec1
shamiulriyad <shamiulriyad1418@gmail.com>
2026-09-26 02:26:05 +0600

    fix frontend and backend bug

--- backend/src/ShilpoHubBD.Application/Interfaces/Repositories/ILiveShoppingRepository.cs
+++ backend/src/ShilpoHubBD.Application/Interfaces/Repositories/ILiveShoppingRepository.cs
@@ -4,7 +4,7 @@ namespace ShilpoHubBD.Application.Interfaces.Repositories;
 
 public interface ILiveShoppingRepository
 {
-    Task<(List<LiveEvent> Items, int TotalCount)> GetPagedAsync(LiveEventStatus? status, int page, int pageSize, CancellationToken cancellationToken);
+    Task<(List<LiveEvent> Items, int TotalCount)> GetPagedAsync(LiveEventStatus? status, int page, int pageSize, CancellationToken cancellationToken, Guid? producerId = null);
     Task<LiveEvent?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
     Task AddAsync(LiveEvent liveEvent, CancellationToken cancellationToken);
     Task AddCommentAsync(LiveEventComment comment, CancellationToken cancellationToken);
