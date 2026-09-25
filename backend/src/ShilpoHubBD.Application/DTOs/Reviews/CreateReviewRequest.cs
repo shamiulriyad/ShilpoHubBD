ae3207de1e48533077cca4e354053cb64cfbffb7
shamiulriyad <shamiulriyad1418@gmail.com>
2026-09-26 02:26:03 +0600

    fix frontend and backend bug

--- backend/src/ShilpoHubBD.Application/DTOs/Reviews/CreateReviewRequest.cs
+++ backend/src/ShilpoHubBD.Application/DTOs/Reviews/CreateReviewRequest.cs
@@ -8,6 +8,7 @@ public class CreateReviewRequest
     public Guid? BookingId { get; set; }
 
     public int Rating { get; set; }
+    public int? ProducerRating { get; set; }
     public string Comment { get; set; } = string.Empty;
     public List<string> ImageUrls { get; set; } = new();
 }
