43a9f95129bed160c0fefa7964d9b75bb26d4ce2
shamiulriyad <shamiulriyad1418@gmail.com>
2026-09-26 22:26:54 +0600

    trip planer fix

--- backend/src/ShilpoHubBD.Application/DTOs/AITourism/TourPlanContext.cs
+++ backend/src/ShilpoHubBD.Application/DTOs/AITourism/TourPlanContext.cs
@@ -16,5 +16,6 @@ public class TourPlanContext
     public List<TouristServiceSummaryDto> Services { get; set; } = new();
     public List<TourismLocationSummaryDto> TourismLocations { get; set; } = new();
     public List<RagTravelNoteDto> RagNotes { get; set; } = new();
+    public DistrictDatasetResult Dataset { get; set; } = new();
     public TransportEstimateDto? TransportEstimate { get; set; }
 }
